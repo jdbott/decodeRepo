5279cad72ad7b71ae79124d42f366752301a200e
AddyVeerendra <addyveerendra@gmail.com>
2025-11-24 18:04:43 -0500

    built in conversion

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/limelight/PoseTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/limelight/PoseTesting.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.vision.limelight;
 
+import com.pedropathing.ftc.FTCCoordinates;
+import com.pedropathing.geometry.PedroCoordinates;
+import com.pedropathing.geometry.Pose;
 import com.qualcomm.hardware.bosch.BHI260IMU;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
@@ -43,37 +46,12 @@ public class PoseTesting extends OpMode {
 
         if (llResult != null && llResult.isValid()) {
             Pose3D botPose = llResult.getBotpose_MT2();
+            Pose pedroPose =  new Pose(botPose.getPosition().x, botPose.getPosition().y, botPose.getOrientation().getYaw(), FTCCoordinates.INSTANCE).getAsCoordinateSystem(PedroCoordinates.INSTANCE);
 
-            final double METERS_TO_INCHES = 39.3701;
-            final double FIELD_LENGTH_METERS = 3.6576;
-            final double HALF_FIELD = FIELD_LENGTH_METERS / 2.0;
+            telemetry.addData("Tag Pose X", pedroPose.getX());
+            telemetry.addData("Tag Pose Y", pedroPose.getY());
+            telemetry.addData("Tag Pose Heading", pedroPose.getHeading());
 
-            double x_m = botPose.getPosition().x;
-            double y_m = botPose.getPosition().y;
-
-            // Shift origin from field center → bottom-left
-            double pedroX_m = x_m;
-            double pedroY_m = y_m + HALF_FIELD;
-
-            // Convert to inches
-            double pedroX_in = pedroX_m * METERS_TO_INCHES;
-            double pedroY_in = pedroY_m * METERS_TO_INCHES;
-
-            // Optional: flip Y if Limelight’s +Y points opposite Pedro’s
-            //pedroY_in = (FIELD_LENGTH_METERS - pedroY_m) * METERS_TO_INCHES;
-
-            // Offset correction (if Limelight not centered)
-            double LL_OFFSET_X = 0.0;
-            double LL_OFFSET_Y = 0.0;
-            pedroX_in += LL_OFFSET_X;
-            pedroY_in += LL_OFFSET_Y;
-
-            telemetry.addData("Pedro X (in)", pedroX_in);
-            telemetry.addData("Pedro Y (in)", pedroY_in);
-            telemetry.addData("Heading (deg)", orientations.getYaw());
-
-            telemetry.addData("Limelight X (meters)", llResult.getBotpose_MT2().getPosition().x);
-            telemetry.addData("Limelight Y (meters)", llResult.getBotpose_MT2().getPosition().y);
         } else {
             telemetry.addData("Tag Pose", "No target found");
         }
