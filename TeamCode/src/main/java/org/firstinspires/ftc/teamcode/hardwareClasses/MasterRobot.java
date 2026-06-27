f7c4a780c016492b4e441408e71b37ba6f9daba9
yuvaang13 <yuvaangulati7689@gmail.com>
2026-06-27 10:27:32 -0400

    the master robot class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardwareClasses/MasterRobot.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.hardwareClasses;
+
+public class MasterRobot {
+    public offLinearSlides slides;
+    public offSeasonIntake intake;
+    public offSeasonDeposit deposit;
+
+}
