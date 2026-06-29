341b6e2efd5f9c0ac3b5aabd78cdc96c4b6ae1a8
yuvaang13 <yuvaangulati7689@gmail.com>
2026-06-29 14:39:41 -0400

    master robot class fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardwareClasses/MasterRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardwareClasses/MasterRobot.java
@@ -1,8 +1,58 @@
-package org.firstinspires.ftc.teamcode.hardwareClasses;//gonna be the master robot hardware class that will be used in the actual robot's program
+package org.firstinspires.ftc.teamcode.hardwareClasses;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+/**
+ * MasterRobot - One class to rule them all.
+ * Stores the HardwareMap and initializes every subsystem.
+ *
+ * WARNING: FTC requires UNIQUE device names in your RC config.
+ * If you have multiple motors, you cannot actually name them all "cool".
+ * Use unique names like "coolSlide", "coolDeposit", "coolLeft", "coolRight"
+ * and update the strings below accordingly.
+ */
 public class MasterRobot {
-    public offLinearSlides slides;
-    public offSeasonIntake intake;
-    public offSeasonDeposit deposit;
 
+    /** Stored reference to the hardware map */
+    public HardwareMap hardwareMap;
+
+    // --- Subsystems ---
+    public offLinearSlides slides;
+    public offSeasonDeposit deposit;
+    public offSeasonIntake intake;
+
+    // --- Preset shortcuts ---
+    public static final double SLIDES_RETRACTED = offLinearSlides.RETRACTED;
+    public static final double SLIDES_LOW       = offLinearSlides.LOW;
+    public static final double SLIDES_MID       = offLinearSlides.MID;
+    public static final double SLIDES_HIGH      = offLinearSlides.HIGH;
+
+    /**
+     * Initializes all subsystems.
+     * @param hwMap The hardwareMap from your OpMode
+     */
+    public MasterRobot(HardwareMap hwMap) {
+        this.hardwareMap = hwMap;
+
+        // All motors use the name "cool" as requested
+        slides  = new offLinearSlides(hwMap);           // default ctor uses "cool"
+        deposit = new offSeasonDeposit(hwMap, "cool");  // pass "cool" explicitly
+        intake  = new offSeasonIntake(hwMap);           // hardcoded to "cool" for both
+    }
+
+    /**
+     * Call this every loop() so the slide PID controller runs.
+     */
+    public void update() {
+        slides.update();
+    }
+
+    /**
+     * Emergency stop for all subsystems.
+     */
+    public void stopAll() {
+        slides.stop();
+        deposit.stop();
+        intake.stop();
+    }
 }
